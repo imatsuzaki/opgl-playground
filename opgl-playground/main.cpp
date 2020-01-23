76b8aa6c2d58753560ec42e079b9fe318d75ef45
imatsuzaki <yudeayase@gmail.com>
2020-01-23 12:08:41 +0900

    Initial Commit

---
+++ opgl-playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opgl-playground
+//
+//  Created by 松崎 出愛 on 2020/01/23.
+//  Copyright © 2020 yudeayase. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
