fa2f529b6ef7804b915366ad2d798aff8a912fdf
imatsuzaki <yudeayase@gmail.com>
2020-01-23 13:00:06 +0900

    Initial commit

--- opgl-playground/main.cpp
+++ opgl-playground/main.cpp
@@ -7,9 +7,30 @@
 //
 
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    // Init GLFW
+    if (glfwInit() == GL_FALSE)
+    {
+        // Failed to init
+        std::cerr << "Can't initialize GLFW" << std::endl;
+        return 1;
+    }
+    
+    GLFWwindow *const window(glfwCreateWindow(640, 480, "Hello!", NULL, NULL));
+    if (window == NULL)
+    {
+        std::cerr << "Can't create GLFW window." << std::endl;
+        return 1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    
+    while (glfwWindowShouldClose(window) == GL_FALSE) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwWaitEvents();
+    }
 }
