3c0f8ea61f2e540b02e0a8f20a88944fb163a780
imatsuzaki <yudeayase@gmail.com>
2020-01-24 18:51:12 +0900

    Add compile shader program

--- opgl-playground/main.cpp
+++ opgl-playground/main.cpp
@@ -8,8 +8,53 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+// Show result of shader object compile check
+GLboolean printShaderInfoLog(GLuint shader, const char *str)
+{
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) std::cerr << "Compile Error in " << str << std::endl;
+    
+    GLsizei bufSize;
+    
+    return static_cast<GLboolean>(status);
+}
+
+// Create program object
+// vsrc: program of vertex shader
+// fsrc: program of fragmetn shader
+GLuint createProgram(const char *vsrc, const char *fsrc){
+    // empty program object
+    const GLuint program(glCreateProgram());
+    
+    if (vsrc != NULL) {
+        // create vertex shader program object
+        const GLuint vobj(glCreateShader(GL_VERTEX_SHADER));
+        glShaderSource(vobj, 1, &vsrc, NULL);
+        glCompileShader(vobj);
+        glAttachShader(program, vobj);
+        glDeleteShader(vobj);
+    }
+    
+    if (fsrc != NULL) {
+        // create fragment shader program object
+        const GLuint fobj(glCreateShader(GL_FRAGMENT_SHADER));
+        glShaderSource(fobj, 1, &fsrc, NULL);
+        glCompileShader(fobj);
+        glAttachShader(program, fobj);
+        glDeleteShader(fobj);
+    }
+    
+    glBindAttribLocation(program, 0, "position");
+    glBindFragDataLocation(program, 0, "fragment");
+    glLinkProgram(program);
+    
+    return program;
+}
 
 int main(int argc, const char * argv[]) {
     // Init GLFW
@@ -44,8 +89,29 @@ int main(int argc, const char * argv[]) {
     
     glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
     
+    static constexpr GLchar vsrc[] =
+      "#version 150 core\n"
+      "in vec4 position;\n"
+      "void main()\n"
+      "{\n"
+      " gl_Position = position;\n"
+      "}\n";
+    
+    static constexpr GLchar fsrc[] =
+    "#version 150 core\n"
+    "in vec4 position;\n"
+    "void main()\n"
+    "{\n"
+    " fragment = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "}\n";
+    
+    const GLuint program(createProgram(vsrc, fsrc));
+    
     while (glfwWindowShouldClose(window) == GL_FALSE) {
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(program);
+        
         glfwSwapBuffers(window);
         glfwWaitEvents();
     }
